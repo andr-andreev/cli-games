fa3495982f40b8e718e3efb201083b1d76b9ab5a
Andrey Andreev <andr-andreev@users.noreply.github.com>
2018-10-10 08:12:20 -0500

    Rework Slot class

--- src/main/java/games/Slot.java
+++ src/main/java/games/Slot.java
@@ -3,70 +3,48 @@ package games;
 public class Slot {
     private static final int SIZE = 7;
 
-    private int balance;
-    private int bet;
-    private int prize;
-
-    private int drum1 = 0;
-    private int drum2 = 0;
-    private int drum3 = 0;
-
     public static void main(String... __) {
-        Slot game = new Slot(100, 10, 1000);
+        int balance = 100;
+        int bet = 10;
+        int prize = 1000;
 
-        game.play();
-    }
+        int drum1 = 0;
+        int drum2 = 0;
+        int drum3 = 0;
 
-    public Slot(final int balance, final int bet, final int prize) {
-        this.balance = balance;
-        this.bet = bet;
-        this.prize = prize;
-    }
+        while (balance > 0) {
+            System.out.println(String.format("Your balance is $%d. Your bet is $%d.", balance, bet));
 
-    public void play() {
-        while (getBalance() > 0) {
-            System.out.println(String.format("Your balance is $%d. Your bet is $%d.", getBalance(), bet));
-            int result = spin();
+            drum1 = getNextValue(drum1);
+            drum2 = getNextValue(drum2);
+            drum3 = getNextValue(drum3);
 
             System.out.println("Spin result:");
             System.out.println(String.format("The 1st drum - %d, the 2nd drum - %d, the 3rd drum - %d", drum1, drum2, drum3));
 
-            modifyBalance(result);
+            int result = isWinningCombination(drum1, drum2, drum3) ? prize : -bet;
+            balance += result;
 
             System.out.println(
                     String.format(
                             "You %s $%d. Your balance is: $%d.%n",
                             result > 0 ? "won" : "lost",
                             Math.abs(result),
-                            getBalance()
+                            balance > 0 ? balance : 0
                     )
             );
         }
     }
 
-    private int getBalance() {
-        return balance > 0 ? balance : 0;
+    private static boolean isWinningCombination(int drum1, int drum2, int drum3) {
+        return drum1 == drum2 && drum1 == drum3;
     }
 
-    private void modifyBalance(final int amount) {
-        balance += amount;
-    }
-
-    private int spin() {
-        drum1 = getNextValue(drum1);
-        drum2 = getNextValue(drum2);
-        drum3 = getNextValue(drum3);
-
-        boolean isWinningCombination = drum1 == drum2 && drum1 == drum3;
-
-        return isWinningCombination ? prize : -bet;
-    }
-
-    private int getNextValue(final int currentValue) {
+    private static int getNextValue(final int currentValue) {
         return (currentValue + getMovementValue()) % SIZE;
     }
 
-    private int getMovementValue() {
+    private static int getMovementValue() {
         return (int) Math.round(Math.random() * 100);
     }
 }
