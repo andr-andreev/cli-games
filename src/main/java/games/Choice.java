96da77d4646a91ae9a9040eca7eb74647eec44b9
Andrey Andreev <andr-andreev@users.noreply.github.com>
2018-10-11 22:52:30 -0500

    Fix games menu

--- src/main/java/games/Choice.java
+++ src/main/java/games/Choice.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 public class Choice {
     public static void main(String... __) throws IOException {
-        System.out.println("Please select a game:");
-        System.out.println("1. Slot machine");
-        System.out.println("2. Drunkard");
+        System.out.println(
+                "Please select a game:\n" +
+                        "1. Slot machine\n" +
+                        "2. Drunkard\n"
+        );
 
         switch (System.in.read()) {
             case '1':
