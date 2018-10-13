9ea7eceef5ee885398a6f7249dfe0771ae2ea606
Andrey Andreev <andr-andreev@users.noreply.github.com>
2018-10-13 00:28:46 -0500

    Add Blackjack game to the menu

--- src/main/java/games/Choice.java
+++ src/main/java/games/Choice.java
@@ -3,22 +3,37 @@ package games;
 import java.io.IOException;
 
 public class Choice {
+    static final String LINE_SEPARATOR = System.getProperty("line.separator");
+
     public static void main(String... __) throws IOException {
         System.out.println(
                 "Please select a game:\n" +
                         "1. Slot machine\n" +
-                        "2. Drunkard\n"
+                        "2. Drunkard\n" +
+                        "3. Blackjack\n"
         );
 
-        switch (System.in.read()) {
+        switch (getCharacterFromUser()) {
             case '1':
                 Slot.main();
                 break;
             case '2':
                 Drunkard.main();
                 break;
+            case '3':
+                Blackjack.main();
+                break;
             default:
                 System.out.println("Invalid selection");
         }
+    }
+
+    static char getCharacterFromUser() throws IOException {
+        byte[] input = new byte[1 + LINE_SEPARATOR.length()];
+        if (System.in.read(input) != input.length) {
+            throw new RuntimeException("Invalid input");
+        }
+
+        return (char) input[0];
     }
 }
