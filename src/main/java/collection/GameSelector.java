241b3d6f7fd76b1b4fa752665b35d9f65fedfef2
Andrey Andreev <andr-andreev@users.noreply.github.com>
2020-08-05 22:26:27 -0500

    Refactor GameSelector.java

--- src/main/java/collection/GameSelector.java
+++ src/main/java/collection/GameSelector.java
@@ -1,28 +1,31 @@
-package games;
+package collection;
 
+import collection.games.Blackjack;
+import collection.games.War;
+import collection.games.SlotMachine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
-public class Choice {
-    private static final Logger logger = LoggerFactory.getLogger(Choice.class);
+public class GameSelector {
+    private static final Logger logger = LoggerFactory.getLogger(GameSelector.class);
 
     static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     public static void main(String... __) throws IOException {
         logger.info(
                 "Please select a game:\n" +
-                        "1. Slot machine\n" +
-                        "2. Drunkard\n" +
+                        "1. Slot Machine\n" +
+                        "2. War\n" +
                         "3. Blackjack\n"
         );
 
         switch (getCharacterFromUser()) {
             case '1':
-                Slot.main();
+                SlotMachine.main();
                 break;
             case '2':
-                Drunkard.main();
+                War.main();
                 break;
             case '3':
                 Blackjack.main();
@@ -32,7 +35,7 @@ public class Choice {
         }
     }
 
-    static char getCharacterFromUser() throws IOException {
+    public static char getCharacterFromUser() throws IOException {
         byte[] input = new byte[1 + LINE_SEPARATOR.length()];
         if (System.in.read(input) != input.length) {
             throw new RuntimeException("Invalid input");
